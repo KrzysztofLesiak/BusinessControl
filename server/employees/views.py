15ba4295be64b2590c71322a3862b1130a454464
Krzysztof <krzysztoflesiak99@gmail.com>
2023-12-15 19:44:30 +0100

    feat: add routes

--- server/employees/views.py
+++ server/employees/views.py
@@ -1,1 +1,41 @@
-from django.shortcuts import render
+
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
+
+@api_view(["GET"])
+def getRoutes(request):
+    routes = [
+        {
+            'Endpoint': '/employees/',
+            'method': 'GET',
+            'body': None,
+            'description': 'Returns array of employees'
+        },
+        {
+            'Endpoint': '/employees/add',
+            'method': 'POST',
+            'body': {'body': ""},
+            'description': 'Create new employee'
+        },
+        {
+            'Endpoint': '/employees/:id',
+            'method': 'GET',
+            'body': None,
+            'description': 'Returns single employee'
+        },
+        {
+            'Endpoint': '/employees/:id',
+            'method': 'PUT',
+            'body': {'body': ""},
+            'description': 'Update employee'
+        },
+        {
+            'Endpoint': '/employees/:id',
+            'method': 'DELETE',
+            'body': {'body': ""},
+            'description': 'Delete employee'
+        },
+    ]
+
+    return Response(routes)
